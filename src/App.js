68e2ece0c8431de5c5ac59f8034b0f34b5872445
sunilmondal4you <sunilmondal4you@gmail.com>
2023-03-26 10:40:41 +0530

    button and click event

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import "./App.css";
 function App() {
   let title = "Hello World!";
   let textId = "title";
+  let saveBtn = "Save Me";
+  let saveMe = () => {
+    alert("save button clicked!");
+  };
   return (
     <div>
       <h1 id={textId}>{title}</h1>
+      <button type="button" onClick={saveMe}>
+        {saveBtn}
+      </button>
     </div>
   );
 }
