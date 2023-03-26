0c4e6b0bf9c3441b6514c450f23b3dac00639fe4
sunilmondal4you <sunilmondal4you@gmail.com>
2023-03-26 16:23:49 +0530

    way of event call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useRef, useState } from "react";
 
 function App() {
   return (
     <>
-      <Hello></Hello>
       <Component></Component>
+      <Component2></Component2>
+      <Component3></Component3>
     </>
   );
 }
@@ -28,7 +30,60 @@ function Hello() {
 }
 
 function Component() {
-  return <div>Another Component</div>;
+  let [message, setMessage] = useState()
+  let saveMessage = ()=>{
+    let newMessage = document.querySelector("#id1").value;
+    setMessage(newMessage);
+    console.log(newMessage)
+  }
+
+  return (
+    <div>
+      <h1>Set Message option1</h1>
+      <input type="text" id="id1" placeholder="input here"/>
+      <input type="button" value="Save" onClick={saveMessage}/>
+      <div>{message}</div>
+      <hr/>
+    </div>
+  );
+}
+
+function Component2(){
+  let msgRef = useRef();
+  let [message, setMessage] = useState();
+
+  let saveMe = ()=>{
+    let newMsg = msgRef.current.value;
+    console.log(newMsg)
+    setMessage(newMsg); 
+  }
+
+  return(
+    <div>
+      <h1>Set Message option2</h1>
+      <input type="text" ref={msgRef} placeholder="Enter here"/>
+      <input type="button" value="Save" onClick={saveMe}/>
+
+      <div>{message}</div>
+      <hr/>
+    </div>
+  );
+}
+
+function Component3 (){
+  let [message, setMessage] = useState('your message!');
+  let messageChange = (e)=>{
+    let newMsg = e.target.value;
+    setMessage(newMsg);
+  };
+  return(
+    <div>
+      <h1>Set Message option3</h1>
+      <input type="text" value={message} placeholder="enter here" onChange={messageChange}/>
+      <div>{message}</div>
+    <hr/>
+    </div>
+  )
 }
 
 export default App;
